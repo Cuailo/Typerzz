a239c781a6ef4642d43c5ec5cb46a4a82d014698
Calum <calumdavies08@gmail.com>
2025-11-06 19:48:52 +0000

    Fix PHP syntax and improve HTML structure

--- Project/home.php
+++ Project/home.php
@@ -2,10 +2,10 @@
   session_start();
 
     echo time();
-	  header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+	header("Cache-Control: no-store, no-cache, must-revalidate");
     header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
-    header("Pragma: no-cache"); // HTTP/1.0
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+    header("Pragma: no-cache"); 
     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 
   if (!$_SESSION['logged_in']) {
@@ -121,3 +121,4 @@
   </body>
 
   </html>
+
